8c82d768b58ae986e4b3b534c61badf684b12e39
AGABA ANDREW <agabaandre@gmail.com>
2023-09-12 19:33:26 +0300

    error fix

--- resources/views/admin/layouts/partials/top_bar_main.blade.php
+++ resources/views/admin/layouts/partials/top_bar_main.blade.php
@@ -2,14 +2,14 @@
 <div class="main-header main-header-fixed" style="background:rgb(186, 183, 16); color:#FFF !important;">
 	<div class=" container">
 		<div class="main-header-left ">
-			<a class="animated-arrow hor-toggle horizontal-navtoggle"><span></span></a><!-- sidebar-toggle-->
+			<a class="animated-arrow hor-toggle horizontal-navtoggle"><span></span></a><!-- filter: brightness(0) invert(1);"sidebar-toggle-->
 			<a class="header-brand" href="{{ url('/') }}">
-				<img src="{{ asset('assets/mama/images/logo.png') }}" id="change-image" style="filter: brightness(0) invert(1);" width=100>
+				<img src="{{ asset('assets/mama/images/logo.png') }}" id="change-image" style="" width=90>
 			</a>
 		</div>
 		<div class="main-header-center">
 			<div class="responsive-logo">
-				<img src="{{ asset('assets/mama/images/logo.png') }}" id="change-image" style="filter: brightness(0) invert(1);" width=100px>
+				<img src="{{ asset('assets/mama/images/logo.png') }}" id="change-image" style="" width=90px>
 			</div>
 		</div>
 		<div class="main-header-right">
