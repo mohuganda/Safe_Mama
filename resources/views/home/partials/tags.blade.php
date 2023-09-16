091299a404d875ad2dd116a525da65a7a205fb83
AGABA ANDREW <agabaandre@gmail.com>
2023-09-16 22:30:03 +0300

    search tags

--- resources/views/home/partials/tags.blade.php
+++ resources/views/home/partials/tags.blade.php
@@ -8,7 +8,7 @@
     @endphp
 
     @foreach($tags as $tag)
-        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded">{{ $tag->tag_text }}</a>
+        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 mt-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded">{{ $tag->tag_text }}</a>
     @endforeach
 
 </div>
