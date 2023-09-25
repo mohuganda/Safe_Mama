506ef6b4655d2ed8631ff22a270add3da2620c93
AGABA ANDREW <agabaandre@gmail.com>
2023-09-25 13:39:32 +0300

    added app

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -54,7 +54,7 @@
 			<div class="col-lg-4">
 				<!-- Footer Widget Start -->
 				<div class="footer-widget text-center">
-
+                 <p>Developed by the GoU under the Uganda Reproductive Maternal and Child Health Services Improvement Project funded by World Bank and the Government of Sweden</p>
 					  <a href="https://www.worldbank.org/en/country/uganda" class="footer-widget__logo"><img src="{{ asset('assets/mama/images/world_bank.png')}}" alt="Logo" width="80"></a>
 
 					  <a href="https://health.go.ug" class="footer-widget__logo"><img src="{{ asset('assets/mama/images/moh_logo.png')}}" alt="Logo" width="30"></a>
